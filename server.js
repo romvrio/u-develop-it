0f97ad9054371f679d8c4d0c9b3b4e2144779069
Romario <morrisromo9966@gmail.com>
2022-02-09 15:47:27 -0500

    db call to candidates on server

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -6,6 +7,21 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+//connect to the database
+const db = mysql.createConnection({
+    host: 'localhost',
+    //my MySQL username
+    user: 'root',
+    //my MySQL password
+    password: 'Invincible3162!',
+    database: 'election'
+},
+    console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
